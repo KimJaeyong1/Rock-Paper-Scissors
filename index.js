698a044890ee3c3ed2b8e89b93f91cf1c0aa8929
ShenWoo <95727730+KimJaeyong1@users.noreply.github.com>
2022-01-11 23:52:22 +0900

    Update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
-//  Rock Paper Scissors  v5.0  //
-/////////////////////////////////
+/*==============================*
+|   Rock Paper Scissors v 1.0.0 |
+*===============================*/
 
 var ComArea;
 var PlayerArea;
@@ -9,64 +9,77 @@ var random1;
 var random2;
 var random3;
 var com;
+var comSpeech;
 
 window.onload = function() {
     ComArea = document.getElementById("com_area");
     PlayerArea = document.getElementById("player_area");
     ImageButton = document.getElementById("sc_btn");
-    
+    comSpeech = document.getElementById("computer");
 }
 
 function scClick() {
-    PlayerArea.innerHTML="<img src='image/sc.png'/>";
+    PlayerArea.innerHTML="<img id='bp' src='image/sc.png'/>";
     console.log("가위 클릭했다구");
     random1 = Math.floor(Math.random()*3)+1;
 
     switch (random1) {
         case 1 :
-            ComArea.innerHTML="<img src='image/sc.png'/>"
+            ComArea.innerHTML="<img id='bc' src='image/sc.png'/>";
+            comSpeech.innerHTML="가바보신 : 가위!";
             break;
         case 2 :
-            ComArea.innerHTML="<img src='image/ro.png'/>"
+            ComArea.innerHTML="<img id='bc' src='image/ro.png'/>";
+            comSpeech.innerHTML="가바보신 : 바위!";
             break;
         case 3 :
-            ComArea.innerHTML="<img src='image/pa.png'/>"
+            ComArea.innerHTML="<img id='bc' src='image/pa.png'/>";
+            comSpeech.innerHTML="가바보신 : 보!";
             break;
     }
+    return 1;
 }
 
 function roClick() {
-    PlayerArea.innerHTML="<img src='image/ro.png'/>";
+    PlayerArea.innerHTML="<img id='bp' src='image/ro.png'/>";
     console.log("바위 클릭했다구");
     random2 = Math.floor(Math.random()*3)+1;
 
     switch (random2) {
         case 1 :
-            ComArea.innerHTML="<img src='image/sc.png'/>"
+            ComArea.innerHTML="<img id='bc' src='image/sc.png'/>";
+            comSpeech.innerHTML="가바보신 : 가위!";
             break;
         case 2 :
-            ComArea.innerHTML="<img src='image/ro.png'/>"
+            ComArea.innerHTML="<img id='bc' src='image/ro.png'/>";
+            comSpeech.innerHTML="가바보신 : 바위!";
             break;
         case 3 :
-            ComArea.innerHTML="<img src='image/pa.png'/>"
+            ComArea.innerHTML="<img id='bc' src='image/pa.png'/>";
+            comSpeech.innerHTML="가바보신 : 보!";
             break;
     }
+    return 2;
 }
             
 function paClick() {
-    PlayerArea.innerHTML="<img src='image/pa.png'/>";
+    PlayerArea.innerHTML="<img id='bp' src='image/pa.png'/>";
     console.log("보 클릭했다구");
     random3 = Math.floor(Math.random()*3+1);
-    console.log(random3);
+
     switch (random3) {
         case 1 :
-            ComArea.innerHTML="<img src='image/sc.png'/>"
+            ComArea.innerHTML="<img id='bc' src='image/sc.png'/>";
+            comSpeech.innerHTML="가바보신 : 가위!";
             break;
         case 2 :
-            ComArea.innerHTML="<img src='image/ro.png'/>"
+            ComArea.innerHTML="<img id='bc' src='image/ro.png'/>";
+            comSpeech.innerHTML="가바보신 : 바위!";
             break;
         case 3 :
-            ComArea.innerHTML="<img src='image/pa.png'/>"
+            ComArea.innerHTML="<img id='bc' src='image/pa.png'/>";
+            comSpeech.innerHTML="가바보신 : 보!";
             break;
     }
+    return 3;
 }
